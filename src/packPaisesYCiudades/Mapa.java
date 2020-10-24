ba7b884d1f9c0a09143c92c19f2e12526ec4a0b7
leire <leiregarciaupv@gmail.com>
2020-10-24 17:49:23 +0200

    todavía no tengo naxD

--- src/packPaisesYCiudades/Mapa.java
+++ src/packPaisesYCiudades/Mapa.java
@@ -4,11 +4,20 @@ public class Mapa {
     Pais first;
 
     public void eliminaCiudadesQueNoSuperanNHabitantes(Mapa pM, int pN){
-       /* post: elimina todas las ciudades escepto las capitales que no superen
+       /*       pre: el mapa tiene almenos un elemento.
+        *
+        *       post: elimina todas las ciudades escepto las capitales que no superen
         *       N miles de habitantes.
         *       Si l acapital de un pais no figurara en la lista de ciudades,
         *       el puntero correspondiente tendrá valor null
         *       Si un pais se quedara sin ciudades debería ser eliminado del mapa.
         */
+
+        Pais pUnekoa= pM.first;
+        Pais pAurrekoa= null;
+        Ciudad cUnekoa=null;
+        Ciudad cAurrekoa= null;
+
     }
+
 }
