d09a176d2457bbf2db8f9d02e4eaf9363460fb89
Aitor <Usuario@AITOR>
2020-10-25 19:08:38 +0100

    Ezabatusublista

---
+++ src/packEzabatuAzpilista/DoubleLinkedList.java
@@ -0,0 +1,13 @@
+package packEzabatuAzpilista;
+
+public class DoubleLinkedList<T> {
+	DoubleNode<T> first;
+	
+	
+	public void ezabatuLista(DoubleLinkedList<T> azpilista) {
+		/*
+		 * pre: "azpilista" uneko listaren zati bat da "azpilista" zerrendako elementuak ordena berean daude lista nagusian
+		 * post: "azpilista" osoa kendu egin da zerrendatik
+		 */
+	}
+}
---
+++ src/packEzabatuAzpilista/DoubleNode.java
@@ -0,0 +1,7 @@
+package packEzabatuAzpilista;
+
+public class DoubleNode<T> {
+	T data;
+	DoubleNode<T> next;
+	DoubleNode<T> prev;
+}	
