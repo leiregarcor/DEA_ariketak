280e9e2ad39a446e5498f30e1c11782ca4259f10
leire <leiregarciaupv@gmail.com>
2020-11-22 12:38:43 +0100

    ordenaciónRaiz

--- 2. partziala/src/packOrdenaciónPorRaizString/OrdenaciónPorRaizString.java
+++ 2. partziala/src/packOrdenaciónPorRaizString/OrdenaciónPorRaizString.java
@@ -1,10 +1,14 @@
 package packOrdenaciónPorRaizString;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class OrdenaciónPorRaizString {
     public void ordRaiz(String[] a, int maxLetras){
         //pre: maxLetras indica el número de letras de las palabras a ordenar, y tdas las letras son minusculas
         //     entre la 'a' y 'z'.
         //post: a está ordenado ascendentemente
+        Queue<String>[]ilara= new LinkedList<String>[26];
     }
 
 
