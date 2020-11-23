ce17e50f15566f378ea0b7368c6200f9b2a003aa
leire <leiregarciaupv@gmail.com>
2020-11-23 12:16:07 +0100

    AITOr, TK MUCHO MUCHO

--- 2. partziala/src/packKoloreenJokoa/Jokoa.java
+++ 2. partziala/src/packKoloreenJokoa/Jokoa.java
@@ -1,6 +1,7 @@
 package packKoloreenJokoa;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -11,17 +12,58 @@ public class Jokoa {
         int dado2;
     }
 
-    Queue<Integer>[] jokalariak;
+    Queue<Integer>[] jokalariak = new Queue[6];
     // Fitxen koloreak balio osokoen bidez adierazten dira: beltzak 0 eta
     // beste jokalarien kolorea bere posizioarekin bat etorriko da (hau da,
     // 1 jokalariak 1 kolorea, ...)
     Stack<Integer> mahaia;
 
-    public int jokoa(int n, ArrayList<Jokaldi> jokaldiak) {
+
+    public Jokoa() {
+        for(int i=0; i<jokalariak.length; i++){
+            jokalariak[i]= new LinkedList<>();
+        }
+        this.mahaia = new Stack<>();
+    }
+
+    public int jokoa( ArrayList<Jokaldi> jokaldiak) {
         // aurre: n jokalari bakoitzaren hasierako fitxa-kopurua da
         // “jokaldiak” zerrendak partida bateko jokaldiak ditu
         // post: emaitza irabazlearen zenbakia da
+        int ema=0;
+        for(Jokaldi j: jokaldiak){
+            if(j.dado1==6) {
+                //dado1 6 bada
+            }
+            else if(j.dado1%2==0) {
+                //dado1 bikoitia bada
+                if(!jokalariak[j.dado2-1].isEmpty())
+                    mahaia.push(jokalariak[j.dado2-1].remove());
+            }
+            else{
+                //dado2 bakoitia bada
+                if(!mahaia.isEmpty())
+                    jokalariak[j.dado2-1].add(mahaia.pop());
+            }
+        }
+        int oraingoB=0;
+        int emaB=0;
+
+        for(int i=1; i<jokalariak.length; i++){
+            while(!jokalariak[i].isEmpty()){
+                if(jokalariak[i].remove()==0){
+                    oraingoB++;
+                }
+            }
+            if(oraingoB>emaB){
+                emaB=oraingoB;
+                ema= i;
+            }
+        }
+        return ema;
     }
 
+    public static void main(String[] args) {
 
+    }
 }
