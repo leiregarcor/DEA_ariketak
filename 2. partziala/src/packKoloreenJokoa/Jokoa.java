2872a7aa74bd4f755b5519e36febdc84f6b9f5d3
leire <leiregarciaupv@gmail.com>
2020-11-23 11:29:35 +0100

    AITOr, TK MUCHO MUCHO

---
+++ 2. partziala/src/packKoloreenJokoa/Jokoa.java
@@ -0,0 +1,27 @@
+package packKoloreenJokoa;
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Jokoa {
+
+    public class Jokaldi {
+        int dado1;
+        int dado2;
+    }
+
+    Queue<Integer>[] jokalariak;
+    // Fitxen koloreak balio osokoen bidez adierazten dira: beltzak 0 eta
+    // beste jokalarien kolorea bere posizioarekin bat etorriko da (hau da,
+    // 1 jokalariak 1 kolorea, ...)
+    Stack<Integer> mahaia;
+
+    public int jokoa(int n, ArrayList<Jokaldi> jokaldiak) {
+        // aurre: n jokalari bakoitzaren hasierako fitxa-kopurua da
+        // “jokaldiak” zerrendak partida bateko jokaldiak ditu
+        // post: emaitza irabazlearen zenbakia da
+    }
+
+
+}
