2db017ed00a7789d6a1298fcac95b784629c2042
leire <leiregarciaupv@gmail.com>
2021-01-04 12:48:00 +0100

    afaria

--- Azterketa finala/packAfariaBiltzarrean/Afaria.java
+++ Azterketa finala/packAfariaBiltzarrean/Afaria.java
@@ -6,17 +6,24 @@ import java.util.Queue;
 
 public class Afaria<T> {
     int numVertices;
-    T[] vertices;
-    ArrayList<Integer>[]adjList;
+    T[] vertices = (T[]) new Object[]{"jon", "ana", "kepa", "peio", "luis", "maria"};
+    ArrayList<Integer>[]adjList; //nork dago haserretuta norekin (grafoaren loturak)
+
+    public Afaria() {
+        this.numVertices = 6;
+        this.adjList = new ArrayList[numVertices];
+    }
 
     public boolean ahalDaAfaria(){
         //zabaleran:
         Queue<Integer> aztertuGabeak = new LinkedList<>();
         boolean ondoGoaz = true;
         boolean[] aztertuak = new boolean[numVertices];
-        boolean[] jangela = new boolean[numVertices];
+        boolean[] jangela = new boolean[numVertices]; //pertsona bakoitzak ze jangelan egongo den gordetzeko
 
-        for(int k=0; k<aztertuak.length;k++) aztertuak[k] = false;
+        for(int k=0; k<aztertuak.length;k++) { //hasieratu false-ra
+            aztertuak[k] = false;
+        }
         aztertuGabeak.add(0); //aztertuGabeen ilararen bukaeran sartu
         aztertuak[0]= true; //aztertuen array-an true jarri
         jangela[0]= false;
@@ -30,9 +37,10 @@ public class Afaria<T> {
                     aztertuGabeak.add(biziLag);
                     jangela[biziLag]=!jangela[unekoa];
                 }
-                else if (jangela[biziLag]!=jangela[unekoa]){ }
-                else{
-                    ondoGoaz=false;
+                else if (jangela[biziLag]!=jangela[unekoa]){ //ondoz ondoko bi nodoen jangelak desberdinak badira ezer ez
+                }
+                else{ //ondoz ondoko bi nodoen jangelak berdinak badira false
+                        ondoGoaz=false;
                 }
             }
         }
@@ -40,4 +48,8 @@ public class Afaria<T> {
     }
 
 
+    public static void main(String[] args) {
+        Afaria a = new Afaria<String>();
+
+    }
 }
