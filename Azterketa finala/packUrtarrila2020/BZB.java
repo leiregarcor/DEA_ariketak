05b4f0d7950d2c4fd40cf43850b3c1e591f4cede
leire <leiregarciaupv@gmail.com>
2021-01-20 13:39:47 +0100

    sin terminar urtarrila2020

--- Azterketa finala/packUrtarrila2020/BZB.java
+++ Azterketa finala/packUrtarrila2020/BZB.java
@@ -2,16 +2,52 @@ package packUrtarrila2020;
 
     public class BZB {
         BinaryTreeNode<Integer> root;
-        public void add(int v)
-        // Aurrebaldintza: v ez dago zuhaitzean.
-        // Postbaldintza: v zuhaitzean txertatu da
-        // Oharra: gogoratu txertaketaren ondorioz zenbait adabegiren numberOfNodes
-        // atributua eguneratu beharko dela.
-        public void printTartea(int a, int b)
-        // Azalpena: Idatzi irteera estandarrean a eta b tarteko balioa duten nodoen balioak
-        // (a eta b barne) goranzko ordenan.
-        // Oharra: Errekurtsiboki inplementatu eta soilik behar diren nodoak tratatu.
+        public void add(int v) {
+            // Aurrebaldintza: v ez dago zuhaitzean.
+            // Postbaldintza: v zuhaitzean txertatu da
+            // Oharra: gogoratu txertaketaren ondorioz zenbait adabegiren numberOfNodes
+            // atributua eguneratu beharko dela.
+            BinaryTreeNode<Integer> berria = new BinaryTreeNode<>(v);
+            if (root=null){
+                root=berria;
+                root.numberOfNodes= root.numberOfNodes+1;
+            }
+            else {
+                BinaryTreeNode<Integer> unekoa= root;
+                boolean txertatua= false;
+                while (!txertatua){
+                    unekoa.numberOfNodes= unekoa.numberOfNodes+1;
+                    if (v< unekoa.data){
+                        if (unekoa.left==null){
+                            unekoa.left=berria;
+                            txertatua=true;
+                            //unekoa.numberOfNodes= unekoa.numberOfNodes+1;
+                        }
+                        else {
+                            unekoa=unekoa.left;
+                        }
+                    }
+                    else { //v>unekoa.data bada
+                        if (unekoa.right==null){
+                        unekoa.right=berria;
+                        txertatua=true;
+                        //unekoa.numberOfNodes= unekoa.numberOfNodes+1;
+                        }
+                        else {
+                            unekoa=unekoa.right;
+                        }
+                    }
+                }
+            }
+        }
 
+
+        public void printTartea(int a, int b){
+            // Azalpena: Idatzi irteera estandarrean a eta b tarteko balioa duten nodoen balioak
+            // (a eta b barne) goranzko ordenan.
+            // Oharra: Errekurtsiboki inplementatu eta soilik behar diren nodoak tratatu.
+
+        }
 
 
 
@@ -20,5 +56,9 @@ package packUrtarrila2020;
             T data;
             BinaryTreeNode<T> left, right;
             int numberOfNodes;
+
+            public BinaryTreeNode(T v) {
+                data=v;
+            }
         }
 }
