deb6c4b64ba42890b156a22ed5e8a3304c50d7d5
leire <leiregarciaupv@gmail.com>
2021-01-20 12:45:59 +0100

    urtarrila2020

---
+++ Azterketa finala/packUrtarrila2020/BZB.java
@@ -0,0 +1,24 @@
+package packUrtarrila2020;
+
+    public class BZB {
+        BinaryTreeNode<Integer> root;
+        public void add(int v)
+        // Aurrebaldintza: v ez dago zuhaitzean.
+        // Postbaldintza: v zuhaitzean txertatu da
+        // Oharra: gogoratu txertaketaren ondorioz zenbait adabegiren numberOfNodes
+        // atributua eguneratu beharko dela.
+        public void printTartea(int a, int b)
+        // Azalpena: Idatzi irteera estandarrean a eta b tarteko balioa duten nodoen balioak
+        // (a eta b barne) goranzko ordenan.
+        // Oharra: Errekurtsiboki inplementatu eta soilik behar diren nodoak tratatu.
+
+
+
+
+
+        private static class BinaryTreeNode<T> {
+            T data;
+            BinaryTreeNode<T> left, right;
+            int numberOfNodes;
+        }
+}
